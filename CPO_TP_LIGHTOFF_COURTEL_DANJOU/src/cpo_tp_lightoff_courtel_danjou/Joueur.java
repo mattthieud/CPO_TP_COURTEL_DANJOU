31c77068434d70f76991cb947f8fdce84d533a8d
matthieu danjou <matthieu.danjou@epfedu.fr>
2022-12-09 10:54:40 +0100

    creation des classes

---
+++ CPO_TP_LIGHTOFF_COURTEL_DANJOU/src/cpo_tp_lightoff_courtel_danjou/Joueur.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cpo_tp_lightoff_courtel_danjou;
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Joueur {
+    
+}
---
+++ CPO_TP_LIGHTOFF_COURTEL_DANJOU/src/cpo_tp_lightoff_courtel_danjou/Partie.java
@@ -0,0 +1,14 @@
+package cpo_tp_lightoff_courtel_danjou;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Utilisateur
+ */
+public class Partie {
+    
+}

89096f791be5b8df6b116c4c1860ab45a669a754
jules courtel <jules.courtel@epfedu.fr>
2022-12-15 13:19:49 +0100

    CPO_TP_LIGHTOFF_COURTEL_DANJOU Class Partie ajout "placerCelluleLumineuse" qui permet de générer aléatoirement des cellules lumineuses allumées

--- CPO_TP_LIGHTOFF_COURTEL_DANJOU/src/cpo_tp_lightoff_courtel_danjou/Partie.java
+++ CPO_TP_LIGHTOFF_COURTEL_DANJOU/src/cpo_tp_lightoff_courtel_danjou/Partie.java
@@ -1,5 +1,7 @@
 package cpo_tp_lightoff_courtel_danjou;
 
+import java.util.Random;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -18,6 +20,16 @@ public class Partie {
         JoueurP = J;
         this.plateau = new Grille();
     }
+    
+    public void placerCelluleLumineuse() { 
+        for (int i = 0; i<5; i++){ //ligne 
+           for (int j = 0; j<5; j++){ //colonne
+                if (Math.random()>0.5){ 
+                    grille_jeu.ChangeEtat(i, j);
+                }
+           }
+        }    
+    } 
     
     public void LancerPartie() {
         grille_jeu.afficherGrilleSurConsole();

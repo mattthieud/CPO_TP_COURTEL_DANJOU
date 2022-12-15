faa35e8f7d282bca3bf67afeb648a6a6767509bb
jules courtel <jules.courtel@epfedu.fr>
2022-12-15 14:50:46 +0100

    CPO_TP_LIGHTOFF_COURTEL_DANJOU Class Partie ajout "action" qui permet de changer l'état de 5 (une centrale et 4 adjacentes) cellules lumineuses choisi par l'utilisateur dans la console

--- CPO_TP_LIGHTOFF_COURTEL_DANJOU/src/cpo_tp_lightoff_courtel_danjou/Partie.java
+++ CPO_TP_LIGHTOFF_COURTEL_DANJOU/src/cpo_tp_lightoff_courtel_danjou/Partie.java
@@ -33,6 +33,10 @@ public class Partie {
     
     public void action(int x, int y) {
         grille_jeu.ChangeEtat(x, y);
+        grille_jeu.ChangeEtat(x+1, y);         
+        grille_jeu.ChangeEtat(x-1, y);         
+        grille_jeu.ChangeEtat(x, y+1);         
+        grille_jeu.ChangeEtat(x, y-1);
     }
     
     public void LancerPartie() {

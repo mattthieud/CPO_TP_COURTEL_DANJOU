5ae5c41fd228379bd87e305395c40955cfe71cd7
jules courtel <jules.courtel@epfedu.fr>
2022-12-27 18:59:23 +0100

    CPO_TP_LIGHTOFF_COURTEL_DANJOU Interface_Graphique finalisation du scrollpanel.

--- CPO_TP_LIGHTOFF_COURTEL_DANJOU/src/cpo_tp_lightoff_courtel_danjou/Interface_Graphique.java
+++ CPO_TP_LIGHTOFF_COURTEL_DANJOU/src/cpo_tp_lightoff_courtel_danjou/Interface_Graphique.java
@@ -19,8 +19,7 @@ public class Interface_Graphique extends javax.swing.JFrame {
      * Creates new form Interface_Graphique
      */
     public Interface_Graphique() {
-        initComponents();
-
+        initComponents(); 
         for (int i = 4; i >= 0; i--) {
             for (int j = 0; j < 5; j++) {
                 final int x = i;
